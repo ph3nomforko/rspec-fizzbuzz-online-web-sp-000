1dde9fc69fc224a86194fbfcdf805544c430ace7
Codi Kraus <codi.kraus@gmail.com>
2021-04-21 00:21:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 != 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0
+  elsif int % 5 == 0 && int % 3 != 0
     "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
@@ -14,4 +14,4 @@ end
 
 fizzbuzz(3) # => You should see a return of "Fizz"
 fizzbuzz(5) # => You should see a return of nil
-fizzbuzz()  # => You should get an ArgumentError
+fizzbuzz(4)  # => You should get an ArgumentError
